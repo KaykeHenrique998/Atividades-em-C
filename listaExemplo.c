203b7f91de60d6cb494b91c87d8700067dc0cb2a
Kayke Henrique <kaykehcaraca@gmail.com>
2026-09-01 20:00:59 -0300

    Create listaExemplo.c

---
+++ listaExemplo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+ int lista_numerica[5];
+
+ lista_numerica[0] =50;
+ lista_numerica[1] = 80;
+ lista_numerica[1] = 70;
+ lista_numerica[1] = 60;
+ lista_numerica[1] = 10;
+
+printf("O seu primeiro valor é: %d\n", lista_numerica[0]);
+printf("O seu segundo valor é: %d\n", lista_numerica[1]);
+printf("O seu terceiro valor é: %d\n", lista_numerica[2]);
+printf("O seu quarto valor é: %d\n", lista_numerica[3]);
+printf("O seu quinto valor é: %d\n", lista_numerica[4]);
+
+
+
+
+}
+
